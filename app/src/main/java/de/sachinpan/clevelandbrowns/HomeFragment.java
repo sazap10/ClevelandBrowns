e55da584978defbebfe628b1d3b625720501f91b
Sachin Pande <sazap10@gmail.com>
2014-11-04 22:03:02 +0000

    Initial commit

---
+++ app/src/main/java/de/sachinpan/clevelandbrowns/HomeFragment.java
@@ -0,0 +1,19 @@
+package de.sachinpan.clevelandbrowns;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+    public HomeFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
